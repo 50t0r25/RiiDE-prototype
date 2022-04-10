222792a6c6c07edf8106fa5de5a1373e69f1d158
Ryan <102423466+50t0r25@users.noreply.github.com>
2022-04-11 00:05:08 +0100

    Bug fixes & improvements (Warning to me): Fixed a bug where default time value when creating a trip was putting zeros in the DB It now checks if the hours and minutes aren't in the past Added onFailureListeners to the trip details class Also added comments to it Warning: Found a database breaking bug that needs to be fixed!!!!!! (no offline failure checks on queries)

--- app/src/main/java/com/ryantest/prototype01/TripDetailsFragment.kt
+++ app/src/main/java/com/ryantest/prototype01/TripDetailsFragment.kt
@@ -47,18 +47,22 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
             parentFragmentManager.popBackStack()
         }
 
-        // TODO: Comment code
-        // TODO: Add failure listeners
         (activity as MainActivity).createLoadingDialog()
+        // Get the current user data from DB or Cache
         db.collection("users").document(auth.currentUser!!.uid).get(Source.CACHE)
             .addOnSuccessListener { user ->
                 var isDriver = false
+                // Get the tripID that the user is currently in
                 val tripID = user.data?.get("currentTripID").toString()
                 db.collection("trips").document(tripID).get()
                     .addOnSuccessListener { trip ->
+                        // Save the trip data to their variables
+                        // TODO: save passengers data
+                        // Since Date and Driver are Object fields, we cast them as HashMaps
                         val tripData = trip.data
                         val date = tripData?.get("date") as HashMap<*, *>
                         val driver = tripData["driver"] as HashMap<*, *>
+                        // Check if the user is the current driver
                         if (driver["userID"]?.equals(auth.currentUser!!.uid)!!) isDriver = true
 
                         (activity as MainActivity).displayTimeInTv(timeTv,
@@ -67,8 +71,11 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
                             date["day"].toString().toInt(),
                             date["month"].toString().toInt(),
                             date["year"].toString().toInt())
+
                         departureTv.text = tripData["departure"].toString()
                         destinationTv.text = tripData["destination"].toString()
+
+                        // If the user is the driver, adds "(You)" next to their username
                         if (isDriver) {
                             driverUsernameTv.text = driver["username"].toString().plus(" (You)")
                         } else {
@@ -77,18 +84,26 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
                         seatsLeftTv.text = "${(tripData["seatsLeft"].toString().toInt() - tripData["maxPassengers"].toString().toInt())}/${tripData["maxPassengers"]}"
                         priceTv.text = tripData["price"].toString().plus(" DZD")
 
+                        // Delete button clicked
+                        // Display a warning, if user presses positive button, delete the trip from the DB
+                        // Prior to deleting, we have to flag driver and all passengers as no longer in a trip
                         deleteButton.setOnClickListener {
+                            // Display the warning
                             MaterialAlertDialogBuilder(requireContext())
                                 .setTitle("Warning")
                                 .setMessage("Are you sure you want to delete this trip?\nthis will also kick all the passengers.")
                                 .setNeutralButton("Cancel") { dialog, _ ->
+                                    // User clicks cancel
                                     dialog.dismiss()
                                 }
                                 .setPositiveButton("Delete") { dialog, _ ->
+                                    // User clicks delete
                                     (activity as MainActivity).createLoadingDialog()
                                     // TODO: Kick passengers out of trip
-                                    db.collection("users").document(auth.currentUser!!.uid)
-                                        .update("isInTrip", false).addOnSuccessListener {
+                                    // Flag user as no longer in a trip
+                                    db.collection("users").document(auth.currentUser!!.uid).update("isInTrip", false)
+                                        .addOnSuccessListener {
+                                            // Delete this trip's document from the DB
                                             db.collection("trips").document(tripID).delete()
                                                 .addOnSuccessListener {
                                                     (activity as MainActivity).dismissLoadingDialog()
@@ -101,12 +116,39 @@ class TripDetailsFragment : Fragment(R.layout.fragment_trip_details) {
                                                     dialog.dismiss()
                                                 }
                                         }
+                                        .addOnFailureListener {
+                                            // Failed setting user as no longer in trip
+                                            (activity as MainActivity).dismissLoadingDialog()
+                                            Toast.makeText(
+                                                context,
+                                                "Failed to access the database\n" + it.localizedMessage,
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
                                 }
                                 .show()
                         }
 
                         (activity as MainActivity).dismissLoadingDialog()
                     }
+                    .addOnFailureListener {
+                        // Cannot access trip data
+                        (activity as MainActivity).dismissLoadingDialog()
+                        Toast.makeText(
+                            context,
+                            "Failed to access the database\n" + it.localizedMessage,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+            }
+            .addOnFailureListener {
+                // Can't access cached database to fetch user data
+                (activity as MainActivity).dismissLoadingDialog()
+                Toast.makeText(
+                    context,
+                    "Failed to access the database\n" + it.localizedMessage,
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
     }
