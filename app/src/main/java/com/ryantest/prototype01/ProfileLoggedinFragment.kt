ba8dcd20ba7d0b824ed035a5809b4f72f704c7e0
Ryan <102423466+50t0r25@users.noreply.github.com>
2022-04-29 19:24:58 +0100

    Improved and added functionalities to the profile tab

--- app/src/main/java/com/ryantest/prototype01/ProfileLoggedinFragment.kt
+++ app/src/main/java/com/ryantest/prototype01/ProfileLoggedinFragment.kt
@@ -1,11 +1,14 @@
 package com.ryantest.prototype01
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -21,9 +24,11 @@ class ProfileLoggedinFragment : Fragment(R.layout.fragment_profile_loggedin) {
     private lateinit var logoutButton : Button
     private lateinit var tripDetailsButton : Button
     private lateinit var openProfileButton : Button
-    private lateinit var userEmailTv : TextView
-    private lateinit var userNameTv : TextView
+    private lateinit var openGithubButton : Button
+    private lateinit var helpButton : Button
+
     private lateinit var isInTripTv : TextView
+    private lateinit var welcomeTv : TextView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -34,12 +39,12 @@ class ProfileLoggedinFragment : Fragment(R.layout.fragment_profile_loggedin) {
         logoutButton = requireView().findViewById(R.id.logoutButton)
         tripDetailsButton = requireView().findViewById(R.id.tripDetailsButton)
         openProfileButton = requireView().findViewById(R.id.openProfileInfoButton)
-        userEmailTv = requireView().findViewById(R.id.userEmailTv)
-        userNameTv = requireView().findViewById(R.id.userNameTv)
+        openGithubButton = requireView().findViewById(R.id.githubButton)
+        helpButton = requireView().findViewById(R.id.helpButton)
         isInTripTv = requireView().findViewById(R.id.isInTripTv)
+        welcomeTv = requireView().findViewById(R.id.welcomeProfileTv)
 
-        userEmailTv.text = (activity as MainActivity).userEmail
-        userNameTv.text = (activity as MainActivity).username
+        welcomeTv.text = "Welcome to RiiDE, ${(activity as MainActivity).username} !"
 
         // Display the trip user state from the cached variable
         if ((activity as MainActivity).isInTrip) {
@@ -48,6 +53,12 @@ class ProfileLoggedinFragment : Fragment(R.layout.fragment_profile_loggedin) {
         } else {
             isInTripTv.text = "You currently aren't in a trip"
             tripDetailsButton.visibility = View.GONE
+        }
+
+        openGithubButton.setOnClickListener {
+            val openURL = Intent(Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://github.com/50t0r25/RiiDE-prototype")
+            startActivity(openURL)
         }
 
         openProfileButton.setOnClickListener {
@@ -62,6 +73,7 @@ class ProfileLoggedinFragment : Fragment(R.layout.fragment_profile_loggedin) {
                 if (user.data?.get("isInTrip").toString().toBoolean()) {
                     // User is in a trip
                     (activity as MainActivity).isInTrip = true
+                    isInTripTv.textAlignment = TextView.TEXT_ALIGNMENT_TEXT_START
                     isInTripTv.text = "Current trip details:"
                     tripDetailsButton.visibility = View.VISIBLE
 
@@ -71,7 +83,8 @@ class ProfileLoggedinFragment : Fragment(R.layout.fragment_profile_loggedin) {
                 } else {
                     // User isn't currently in a trip
                     (activity as MainActivity).isInTrip = false
-                    isInTripTv.text = "You are currenly not in a trip"
+                    isInTripTv.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+                    isInTripTv.text = "You currently aren't in a trip,\nJoin or create one!"
                     tripDetailsButton.visibility = View.GONE
                 }
 
@@ -88,6 +101,15 @@ class ProfileLoggedinFragment : Fragment(R.layout.fragment_profile_loggedin) {
             FirebaseAuth.getInstance().signOut()
             (activity as MainActivity).refreshMainActivity()
             (activity as MainActivity).setCurrentFragment(ProfileFragment())
+        }
+
+        helpButton.setOnClickListener {
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle("What is RiiDE?")
+                .setMessage("RiiDE is an algerian local carpooling app, it provides you the ability to create and share, or join trips.\nTo start using it, either search for a trip, join it then contact the driver via the info on their profile, or make your own trip and wait for people to join it.\n\nThis app was made as an end of cycle project for our bachelor's CS degree.\n\nGithub: @50t0r25 @windyznuts")
+                .setPositiveButton("Close", null)
+                .show()
         }
     }
 }
